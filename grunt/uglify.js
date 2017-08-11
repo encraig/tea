83a619471f9f10cc25d56dc89da0dd0309481286
David Thanphilom <david.thanphilom@teacollection.com>
2017-08-10 17:35:06 -0700

    added nav redesign files

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -5,9 +5,16 @@ module.exports = function(grunt){
         	sourceMap: true
       	},
 
-      	myScripts:{
+      	navRedesignScripts:{
         	files:{
-          		// 'build/js/myscripts.min.js':['build/js/myscripts.js']
+          		'nav-redesign/js/output/nav1.min.js':'nav-redesign/js/nav1.js',
+              'nav-redesign/js/output/nav2.min.js':'nav-redesign/js/nav2.js',
+              'nav-redesign/js/output/nav3.min.js':'nav-redesign/js/nav3.js',
+              'nav-redesign/js/output/nav4.min.js':'nav-redesign/js/nav4.js',
+              'nav-redesign/js/output/nav5.min.js':'nav-redesign/js/nav5.js',
+              'nav-redesign/js/output/nav6.min.js':'nav-redesign/js/nav6.js',
+              'nav-redesign/js/output/nav7.min.js':'nav-redesign/js/nav7.js',
+              'nav-redesign/js/output/nav8.min.js':'nav-redesign/js/nav8.js',
         	}
       	},
   	}
