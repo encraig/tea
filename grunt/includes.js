8a964a5067063ef4d0c1d2123af42515b561696c
David Thanphilom <david.thanphilom@teacollection.com>
2017-08-30 11:30:44 -0700

    added landingPagesIncludes

--- grunt/includes.js
+++ grunt/includes.js
@@ -8,9 +8,6 @@ module.exports = function(grunt){
 
 		// HOMEPAGE  ---------------------------------------------------------------------------
 		hpIncludes: {
-			options: {
-				includePath: 'homepage/170907/include',
-			},
 
 			files: [
 				//toolkit
@@ -21,9 +18,6 @@ module.exports = function(grunt){
 
 		// CATEGORIES  ---------------------------------------------------------------------------
 		categoriesIncludes: {
-			options: {
-				includePath: 'category-pages/170907/include',
-			},
 
 			files: [
 				//toolkit
@@ -32,17 +26,37 @@ module.exports = function(grunt){
 
 		},
 
-		// PROMOS  ---------------------------------------------------------------------------
-		promosIncludes: {
-			options: {
-				includePath: 'promos/labor-day/170831/include',
-			},
+		// LANDING PAGES  ---------------------------------------------------------------------------
+		landingPagesIncludes: {
 
 			files: [
-				//build
-				{ cwd: 'promos/labor-day/170831', src: [ 'dev/content-build-d/content.html' ], dest: 'promos/labor-day/170831/build/build-labor-day-d-2.html',},
-				//toolkit
-				{ cwd: 'promos/labor-day/170831', src: [ 'dev/content.html' ], dest: 'promos/labor-day/170831/toolkit/toolkit-labor-day2.html',},
+				// about toolkit
+				{ cwd: 'landing-page/about/dev', src: [ '*.html' ], dest:  'landing-page/about/toolkit/toolkit-about.html'},
+				// baby sweeps toolkit
+				{ cwd: 'landing-page/baby-sweeps/dev', src: [ '*.html' ], dest:  'landing-page/baby-sweeps/toolkit/toolkit-babysweeps.html'},
+				// back to school sweeps toolkit
+				{ cwd: 'landing-page/back-to-school-sweeps/dev', src: [ 'content.html' ], dest:  'landing-page/back-to-school-sweeps/toolkit/toolkit-back-to-school-sweeps.html'},
+				{ cwd: 'landing-page/back-to-school-sweeps/dev', src: [ 'thanks.html' ], dest:  'landing-page/back-to-school-sweeps/toolkit/toolkit-back-to-school-thanks.html'},
+				// careers toolkit
+				{ cwd: 'landing-page/careers/dev', src: [ '*.html' ], dest:  'landing-page/careers/toolkit/toolkit-careers.html'},
+				// influencer toolkit
+				{ cwd: 'landing-page/influencer/170828/dev', src: [ '*.html' ], dest:  'landing-page/influencer/170828/toolkit/toolkit-influencer.html'},
+				// inspiration toolkit
+				{ cwd: 'landing-page/inspiration/scotland/dev', src: [ '*.html' ], dest:  'landing-page/inspiration/scotland/toolkit/toolkit-inspiration.html'},
+				// little citizen toolkit
+				{ cwd: 'landing-page/little-citizen/170723/dev', src: [ '*.html' ], dest:  'landing-page/little-citizen/170723/toolkit/toolkit-little-citizen.html'},
+			],
+
+		},
+
+		// PROMOS  ---------------------------------------------------------------------------
+		promosIncludes: {
+
+			files: [
+				// labor day toolkit
+				{ cwd: 'promos/labor-day/170831/dev', src: [ '*.html' ], dest: 'promos/labor-day/170831/toolkit/toolkit-labor-day.html',},
+				// influencer toolkit
+				{ cwd: 'promos/influencer/170828/dev', src: [ '*.html' ], dest: 'promos/influencer/170828/toolkit/toolkit-influencer.html',},
 			]
 		},
 	}
