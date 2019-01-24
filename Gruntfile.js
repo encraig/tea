8e700fe46b77532b1e23d1d1981b5f4253e95e83
David Thanphilom <davidthanphilom@msn.com>
2019-01-24 13:05:02 -0800

    Gruntfile.js added global master

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,47 +1,68 @@
-module.exports = function(grunt){
+module.exports = function(grunt) {
 
-  	// Default task(s).
-  	grunt.registerTask('hello', function(){
-      console.log('hello world from Grunt!');
-    });
+  // Default task(s).
+  grunt.registerTask('hello', function() {
+    console.log('hello world from Grunt!');
+  });
 
- 	grunt.registerTask('makeCss', function(path1, path2){
-      var data = grunt.file.read(path1);
-      var output = '<style>\n'+data+'\n</style>';
-      grunt.file.write(path2, output);
-    });
+  grunt.registerTask('makeCss', function(path1, path2) {
+    var data = grunt.file.read(path1);
+    var output = '<style>\n' + data + '\n</style>';
+    grunt.file.write(path2, output);
+  });
 
-  	grunt.loadNpmTasks('grunt-contrib-watch');
-  	grunt.loadNpmTasks('grunt-contrib-uglify');
-  	grunt.loadNpmTasks('grunt-contrib-cssmin');
-  	grunt.loadNpmTasks('grunt-contrib-less');
-  	grunt.loadNpmTasks('grunt-contrib-concat');
-  	grunt.loadNpmTasks('grunt-execute');
-  	grunt.loadNpmTasks('grunt-contrib-connect');
-  	grunt.loadNpmTasks('grunt-concurrent');
-  	grunt.loadNpmTasks('grunt-uncss');
-    grunt.loadNpmTasks('grunt-includes');
-    grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-execute');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-uncss');
+  grunt.loadNpmTasks('grunt-includes');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
-  	var gruntConfig = {
-      hp:{date:'default'},
-      cat:{date:'default'},
-      landing:{name:'baby-sweeps-thanks', date:'default'},
-      cell:{name:'tween', date:'default'},
-      zone:{name:'footer'}, 
-      promo: {name:'friendsFamily', date:'default'},
-      nav:{folder:'test2', name:'nav1'},
-    };
+  var gruntConfig = {
+    hp: {
+      date: 'default'
+    },
+    cat: {
+      date: 'default'
+    },
+    landing: {
+      name: 'customer-acquisition',
+      date: 'default'
+    },
+    cell: {
+      name: 'tween',
+      date: 'default'
+    },
+    zone: {
+      name: 'catalogs-request'
+    },
+    promo: {
+      name: 'friendsFamily',
+      date: 'default'
+    },
+    nav: {
+      folder: 'test2',
+      name: 'nav1'
+    },
+    global: {
+      name: 'concat',
+    },
+  };
 
 
-	// load all external grunt plugins
-	var tasks = ['watch','less','concat','uncss', 'cssmin','uglify','execute','connect','concurrent','includes','copy'];
+  // load all external grunt plugins
+  var tasks = ['watch', 'less', 'concat', 'uncss', 'cssmin', 'uglify', 'execute', 'connect', 'concurrent', 'includes', 'copy'];
 
-	tasks.forEach(function(fileName){
-  		gruntConfig[fileName] = require('./grunt/' + fileName + '.js')(grunt);
-	});
+  tasks.forEach(function(fileName) {
+    gruntConfig[fileName] = require('./grunt/' + fileName + '.js')(grunt);
+  });
 
- 	grunt.initConfig(gruntConfig);
+  grunt.initConfig(gruntConfig);
 
-  
+
 };
