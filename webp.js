bedefc72ed73ee9bc062f0c9137d5f81deaf5d10
David Thanphilom <davidthanphilom@msn.com>
2019-02-12 12:07:52 -0800

    webp.js edited code for easy path change master

--- webp.js
+++ webp.js
@@ -12,14 +12,16 @@ var fs = require('fs');
 //   console.log(status, error);
 // });
 
-fs.readdir('homepage/default/images', 'utf8', function(e, f) {
+var path = 'promos/one-offs/default/images/';
+
+fs.readdir(path, 'utf8', function(e, f) {
   // console.log(e, f);
   var images = f;
   images.forEach(function(val, i) {
     // console.log(val);
     var name = val.split('.')[0] + '.webp';
     // console.log(name);
-    webp.cwebp('homepage/default/images/' + val, 'homepage/default/images/' + name, "-q 80", function(status, error) {
+    webp.cwebp(path + val, path + name, "-q 80", function(status, error) {
       // if conversion successful status will be '100'
       // if conversion fails status will be '101'
       console.log(status, error);
