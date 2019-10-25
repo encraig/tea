72a3b0a17776b2ae4ea75d170c159387de2447cd
David Thanphilom <davidthanphilom@msn.com>
2019-10-25 14:31:09 -0700

    homepage/ coded promo.select-styles.10.27

--- homepage/default/js/c1.js
+++ homepage/default/js/c1.js
@@ -1,32 +1,32 @@
 var c1 = [{
     elem: '.slide0 .cta1',
-    url: '/girls-clothing/',
+    url: '/girls-clothing/favorites/limited-time-sale',
     text: 'shop girl'
   },
   {
     elem: '.slide0 .cta2',
-    url: '/boys-clothing/',
+    url: '/boys-clothing/favorites/limited-time-sale',
     text: 'shop boy'
   },
   {
     elem: '.slide0 .cta3',
-    url: '/tween-girls-clothing/',
+    url: '/tween-girls-clothing/favorites/limited-time-sale',
     text: 'shop tween'
   },
   {
     elem: '.slide0 .cta4',
-    url: '/baby-girl-clothes/',
+    url: '/baby-girl-clothes/favorites/limited-time-sale',
     text: 'shop baby girl'
   },
   {
     elem: '.slide0 .cta5',
-    url: '/baby-boy-clothes/',
+    url: '/baby-boy-clothes/favorites/limited-time-sale',
     text: 'shop baby boy'
   },
   {
     elem: '.slide0 .cta6',
     url: '/new-arrivals',
-    text: 'shop now'
+    text: 'shop with code fallfest'
   },
   {
     elem: '.slide1 .cta1',
