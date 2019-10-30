1431e9455c37853cacd33a8630eb4ba612b4fcd3
David Thanphilom <davidthanphilom@msn.com>
2019-10-30 15:23:35 -0700

    homepage/ coded launch.10.31

--- homepage/default/js/alt.js
+++ homepage/default/js/alt.js
@@ -5,15 +5,14 @@ var addAlt = function(elem, alt) {
   });
 };
 
-addAlt('.hp .c1 .slide0 img', 'mix & match flash sale $15 tops & leggings + $25 bottoms');
-addAlt('.hp .c1 .slide1 img', 'boy and girl');
-addAlt('.hp .c1 .slide2 img', 'dresses');
-addAlt('.hp .c1 .slide3 img', 'two girls');
+addAlt('.hp .c1 .slide1 img', 'girl with presents');
+addAlt('.hp .c1 .slide2 img', 'boy & girl in PJs');
+addAlt('.hp .c1 .slide3 img', 'girl & boy');
 addAlt('.hp .c1 .slide4 img', 'kids in pajamas');
 addAlt('.hp .c2 img', 'tea gives back');
-addAlt('.hp .c3 img', 'boy');
-addAlt('.hp .c4 .first img', 'new sale styles');
-addAlt('.hp .c4 .second img', 'girl');
-addAlt('.hp .c5 img', 'baby');
-addAlt('.hp .c6 .second img', 'girls at the market');
+addAlt('.hp .c3 img', 'girl & boy');
+addAlt('.hp .c4 .second img', '2 girls');
+addAlt('.hp .c5 .first img', 'baby');
+addAlt('.hp .c5 .second img', 'baby clothes');
+addAlt('.hp .c6 .second img', '2 girls');
 addAlt('.hp .c7 img', 'little sisters fund');
