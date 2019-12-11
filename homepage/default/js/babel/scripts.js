1169b7bd46bae84ed1ad8277f7b33bcc35f93d56
David Thanphilom <david.thanphilom@teacollection.com>
2019-12-11 14:23:14 -0800

    homepage/ coded launch.12.12

--- homepage/default/js/babel/scripts.js
+++ homepage/default/js/babel/scripts.js
@@ -97,17 +97,19 @@ var addAlt = function addAlt(elem, alt) {
   });
 };
 
-addAlt('.hp .c1 .slide1 img', 'boy and girl');
+addAlt('.hp .c1 .slide0 img', 'girl and boy');
+addAlt('.hp .c1 .slide1 img', 'boy & girl');
 addAlt('.hp .c1 .slide2 img', 'boy & girl in PJs'); // addAlt('.hp .c1 .slide3 img', 'girl & boy');
 // addAlt('.hp .c1 .slide4 img', 'kids in pajamas');
 
 addAlt('.hp .c2 img', 'tea gives back');
-addAlt('.hp .c3 img', 'girl & boy');
-addAlt('.hp .c4 .second img', '2 girls');
-addAlt('.hp .c5 .first img', 'baby');
+addAlt('.hp .c3 .first img', 'girl');
+addAlt('.hp .c3 .second img', 'baby');
+addAlt('.hp .c4 .second img', 'girl');
+addAlt('.hp .c5 .first img', '2 girls');
 addAlt('.hp .c5 .second img', 'baby clothes');
 addAlt('.hp .c6 .second img', '2 girls');
-addAlt('.hp .c7 img', 'little sisters fund');
+addAlt('.hp .c7 img', 'Giving Tuesday: Giving Back for Good');
 //# sourceMappingURL=alt.js.map
 
 "use strict";
