f001b35c3954df3978f967731c1588616f1139ac
David Thanphilom <david.thanphilom@teacollection.com>
2019-12-12 11:36:04 -0800

    promos/cyber-monday pulled from promo.cyber-monday.12.1 master

---
+++ promos/cyber-monday/default/js/babel/scripts.js
@@ -0,0 +1,38 @@
+"use strict";
+
+$(document).ready(function () {
+  //responsive image map
+  //$('img[usemap]').rwdImageMaps();
+  //array of urls to hide the banners
+  var urlArray = [{
+    name: 'hp',
+    url: '/'
+  } // {
+  // 	name: 'black-friday',
+  // 	url: '/black-friday'
+  // },
+  // {
+  // 	name:'sale-page',
+  // 	url:'/clothing-sale'
+  // },
+  ]; //function to hide or show the banner
+
+  var hideBanners = function hideBanners() {
+    var url = window.location.pathname; // $('.the-promo .see-details').fancybox();
+
+    $.each(urlArray, function (i, val) {
+      if (url == val.url) {
+        $('div[data-zone="sitewide_banner"]').empty();
+      }
+    });
+  }; //on page load call hideBanners
+
+
+  hideBanners();
+  $('.the-promo .see-details').fancybox();
+  $('.close-fancy').click(function (e) {
+    // e.preventDefault();
+    $.fancybox.close();
+  });
+});
+//# sourceMappingURL=scripts.js.map
