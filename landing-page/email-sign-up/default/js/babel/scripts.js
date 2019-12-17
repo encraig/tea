55b0c35559d54e8814e04fda939be69635f87fed
David Thanphilom <david.thanphilom@teacollection.com>
2019-12-16 16:59:05 -0800

    landing-page/email-sign-up still coding landing.email.12.17

--- landing-page/email-sign-up/default/js/babel/scripts.js
+++ landing-page/email-sign-up/default/js/babel/scripts.js
@@ -2,23 +2,61 @@
 
 $(document).ready(function () {
   var url = window.location.href;
+  var email = $('.email-wrap').find('input').val();
+  var button = $('.email-wrap').find('button');
+
+  var emailIsValid = function emailIsValid(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  };
+
   var ac;
   var promo;
 
   if (url.indexOf('?') !== -1) {
     var paramArr = url.split('?')[1].split('&');
     $.each(paramArr, function (i, val) {
-      if (val.indexOf('campaign') !== -1) {
+      if (val.indexOf('c') !== -1) {
         var str1 = val.split('=')[1];
         ac = str1;
-      } else if (val.indexOf('promo') !== -1) {
+      } else if (val.indexOf('p') !== -1) {
         var str2 = val.split('=')[1];
         promo = str2;
       }
     });
-    var message = "<p>save ".concat(promo, "% off</p>\n    <p>you're welcome.</p>\n    <p>btw the acquisition source is: ").concat(ac, "</p>\n    ");
+    var message = "<p>Sign up for exclusive offers, new styles, and ".concat(promo, "% off your first order!*</p>");
     $('.email-wrap .default').hide();
     $('.email-wrap .promo-message').append(message);
+    $('.email-wrap .new-customers').show();
+  } else {
+    ac = "email sign up landing page";
   }
+
+  $(button).click(function (e) {
+    e.preventDefault();
+    console.log(email, ac, promo);
+    var valid = emailIsValid(email);
+    console.log('valid:' + valid);
+
+    if (valid) {
+      Sailthru.integration("userSignUp", {
+        "id": email,
+        "email": email,
+        "lists": {
+          // "MASTER_CONTACTS_LIST": 1
+          "test list": 1
+        },
+        "vars": {
+          "ACQUISITION_SOURCE": ac
+        },
+        "source": ac,
+        "onSuccess": function onSuccess() {
+          console.log('sailthru email sign up page success');
+        },
+        "onError": function onError() {
+          console.log('sailthru email sign up page error');
+        }
+      });
+    }
+  });
 });
 //# sourceMappingURL=scripts.js.map
--- landing-page/email-sign-up/default/js/scripts.js
+++ landing-page/email-sign-up/default/js/scripts.js
@@ -1,24 +1,60 @@
 $(document).ready(function() {
   const url = window.location.href;
+  var email = $('.email-wrap').find('input').val();
+  const button = $('.email-wrap').find('button');
+
+  const emailIsValid = (email) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+  }
+
   var ac;
   var promo;
   if (url.indexOf('?') !== -1) {
     let paramArr = url.split('?')[1].split('&');
     $.each(paramArr, function(i, val) {
-      if (val.indexOf('campaign') !== -1) {
+      if (val.indexOf('c') !== -1) {
         var str1 = val.split('=')[1];
         ac = str1;
-      } else if (val.indexOf('promo') !== -1) {
+      } else if (val.indexOf('p') !== -1) {
         var str2 = val.split('=')[1];
         promo = str2;
       }
     });
 
-    const message = `<p>save ${promo}% off</p>
-    <p>you're welcome.</p>
-    <p>btw the acquisition source is: ${ac}</p>
-    `;
+    const message = `<p>Sign up for exclusive offers, new styles, and ${promo}% off your first order!*</p>`;
     $('.email-wrap .default').hide();
     $('.email-wrap .promo-message').append(message);
+    $('.email-wrap .new-customers').show();
+  } else {
+    ac = "email sign up landing page";
   }
+
+  $(button).click(function(e) {
+    e.preventDefault();
+    console.log(email, ac, promo);
+    var valid = emailIsValid(email);
+    console.log('valid:' + valid);
+    if (valid) {
+      Sailthru.integration("userSignUp", {
+        "id": email,
+        "email": email,
+        "lists": {
+          // "MASTER_CONTACTS_LIST": 1
+          "test list": 1
+        },
+        "vars": {
+          "ACQUISITION_SOURCE": ac,
+        },
+        "source": ac,
+        "onSuccess": function() {
+          console.log('sailthru email sign up page success');
+        },
+        "onError": function() {
+          console.log('sailthru email sign up page error');
+        }
+      });
+    }
+  });
+
+
 });
