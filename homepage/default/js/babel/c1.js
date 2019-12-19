931e3374c18c2489d8cf288103880bd51f55d94a
David Thanphilom <davidthanphilom@msn.com>
2019-12-19 13:06:06 -0800

    homepage/ coded c1.js promo.eos.12.26

--- homepage/default/js/babel/c1.js
+++ homepage/default/js/babel/c1.js
@@ -2,69 +2,79 @@
 
 var c1 = [{
   elem: '.slide0 .cta1',
-  url: '/girls-clothing',
+  url: '/sale/girls-clothing/view-all',
   text: '<span>shop</span> girl'
 }, {
   elem: '.slide0 .cta2',
-  url: '/boys-clothing',
+  url: '/sale/boys-clothing/view-all',
   text: '<span>shop</span> boy'
 }, {
   elem: '.slide0 .cta3',
-  url: '/tween-girls-clothing',
+  url: '/sale/tween-girls-clothing/view-all',
   text: '<span>shop</span> tween'
 }, {
   elem: '.slide0 .cta4',
-  url: '/baby-girl-clothes',
+  url: '/sale/baby-girl-clothes/view-all',
   text: '<span>shop</span> baby girl'
 }, {
   elem: '.slide0 .cta5',
-  url: '/baby-boy-clothes',
+  url: '/sale/baby-boy-clothes/view-all',
   text: '<span>shop</span> baby boy'
 }, {
   elem: '.slide0 .cta6',
-  url: '/clothing-sale',
-  text: '<span>shop</span> sale'
-}, {
-  elem: '.slide1 .cta1',
-  url: '/baby-girl-clothes/new-arrivals',
-  text: '<span>shop</span> baby girl'
-}, {
-  elem: '.slide1 .cta2',
-  url: '/baby-boy-clothes/new-arrivals',
-  text: '<span>shop</span> baby boy'
-}, {
-  elem: '.slide1 .cta3',
-  url: '/newborn-clothes/favorites/new-arrivals',
+  url: '/sale/newborn-clothing/view-all',
   text: '<span>shop</span> newborn'
-}, {
-  elem: '.slide3 .cta1',
-  url: '/girls-clothing/new-arrivals',
-  text: '<span>shop</span> girl'
-}, {
-  elem: '.slide3 .cta2',
-  url: '/boys-clothing/new-arrivals',
-  text: '<span>shop</span> boy'
-}, {
-  elem: '.slide3 .cta3',
-  url: '/tween-girls-clothing/favorites/new-arrivals',
-  text: '<span>shop</span> tween'
-}, {
-  elem: '.slide3 .cta4',
-  url: '/baby-girl-clothes/new-arrivals',
-  text: '<span>shop</span> baby girl'
-}, {
-  elem: '.slide3 .cta5',
-  url: '/baby-boy-clothes/new-arrivals',
-  text: '<span>shop</span> baby boy'
-}, {
-  elem: '.slide3 .cta6',
-  url: '/newborn-clothes/favorites/new-arrivals',
-  text: '<span>shop</span> newborn'
-}, {
-  elem: '.slide2 .cta1',
-  url: '/pajamas',
-  text: 'shop pjs'
 } // {
+//   elem: '.slide1 .cta1',
+//   url: '/baby-girl-clothes/new-arrivals',
+//   text: '<span>shop</span> baby girl'
+// },
+// {
+//   elem: '.slide1 .cta2',
+//   url: '/baby-boy-clothes/new-arrivals',
+//   text: '<span>shop</span> baby boy'
+// },
+// {
+//   elem: '.slide1 .cta3',
+//   url: '/newborn-clothes/favorites/new-arrivals',
+//   text: '<span>shop</span> newborn'
+// },
+// {
+//   elem: '.slide3 .cta1',
+//   url: '/girls-clothing/new-arrivals',
+//   text: '<span>shop</span> girl'
+// },
+// {
+//   elem: '.slide3 .cta2',
+//   url: '/boys-clothing/new-arrivals',
+//   text: '<span>shop</span> boy'
+// },
+// {
+//   elem: '.slide3 .cta3',
+//   url: '/tween-girls-clothing/favorites/new-arrivals',
+//   text: '<span>shop</span> tween'
+// },
+// {
+//   elem: '.slide3 .cta4',
+//   url: '/baby-girl-clothes/new-arrivals',
+//   text: '<span>shop</span> baby girl'
+// },
+// {
+//   elem: '.slide3 .cta5',
+//   url: '/baby-boy-clothes/new-arrivals',
+//   text: '<span>shop</span> baby boy'
+// },
+// {
+//   elem: '.slide3 .cta6',
+//   url: '/newborn-clothes/favorites/new-arrivals',
+//   text: '<span>shop</span> newborn'
+// },
+// {
+//   elem: '.slide2 .cta1',
+//   url: '/pajamas',
+//   text: 'shop pjs'
+// },
+// {
 //   elem: '.slide3 .cta1',
 //   url: '/tee-shop',
 //   text: 'shop graphic tees'
